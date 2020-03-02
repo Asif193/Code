1e3fd2f0deeb4f8604880c05ca68a68e23413c32
Asif193 <61371127+Asif193@users.noreply.github.com>
2020-03-02 21:20:44 +0600

    Add files via upload

---
+++ Maximum between three number.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+main ()
+{
+    int a,b,c;
+        printf("Enter three number:");
+        scanf("%d%d%d", &a, &b, &c);
+        if (a>b && a>c)
+        {
+            printf("%d is the largest number", a);
+
+        }
+        else if (b>c)
+        {
+            printf("%d is the largest number", b);
+        }
+        else
+        {
+            printf("%d is the largest number", c);
+        }
+
+        return 0;
+
+}
