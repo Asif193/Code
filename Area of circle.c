46e2c14e5b90cbe608a6d320aa919f4cf76cab9f
Asif193 <61371127+Asif193@users.noreply.github.com>
2020-03-02 21:17:04 +0600

    Add files via upload

---
+++ Area of circle.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+
+
+float pi =3.14;
+float radius,area;
+printf("Inter the radius of circle");
+scanf("%f", &radius);
+
+area= pi*( radius * radius);
+
+printf("Area=%.2f\n", area);
+
+return 0;
+
+}
